b7b5f3c3d465f39c9b1dd75e547077410e580c4a
kaito hasegawa <kaitohasegawa000@gmail.com>
2020-04-15 21:22:31 +0900

    修正

--- src/gui.py
+++ src/gui.py
@@ -12,6 +12,7 @@ import array
 #MacやLinuxの場合は、↑３つの代わりに
 import re
 import subprocess
+import shutil
 
 # from subprocess import Popen, PIPE
 # cmd = "xwininfo -name (ウインドウ名)"
@@ -20,9 +21,20 @@ import subprocess
 # coord = re.search('X:\s+(\d+)[^Y]+Y:\s+(\d+)',ret)
 # appwin_x,appwin_y = coord.groups()
 
-pyautogui.moveTo(1, 1, duration=1)
+
+
+target_dir = 'images/active'
+time.sleep(1)
+shutil.rmtree(target_dir)
+os.mkdir(target_dir)
+time.sleep(1)
+new_path = shutil.move('images/stock/image' + "1", 'images/active/')
+time.sleep(1)
+
+
+pyautogui.moveTo(1, 1, duration=0)
 time.sleep(0.5)
 screen_x,screen_y = pyautogui.size()
-pyautogui.moveRel(1370, 40, 1)
+pyautogui.moveRel(1370, 40, 0)
 time.sleep(0.5)
-pyautogui.dragTo(400, 400, 1, button='left')
+pyautogui.dragTo(400, 400, 0.5, button='left')
--- src/ya.py
+++ src/ya.py
@@ -13,17 +13,20 @@ import subprocess
 import sys
 import time
 import array
+import shutil
 
 
 # ブラウザ操作
-mail_adress = "akitan_neru@yahoo.co.jp"
-pass_ward = "Hamagonn5"
+mail_adress = "ynck630vwte@yahoo.co.jp"
+pass_ward = "dmvb@15773"
 options = Options()
 driver = webdriver.Chrome(chrome_options=options)
+
+# 出品画面にアクセス
 url = "https://auctions.yahoo.co.jp/sell/jp/show/submit?category=0"
 driver.get(url)
+driver.delete_all_cookies()
 time.sleep(5)
-# driver.find_element_by_class_name('t_jb9bKlgIcajcRS2hZAP').click();
 element = driver.find_element_by_id("username");
 element.send_keys(mail_adress)
 time.sleep(3)
@@ -33,9 +36,35 @@ element = driver.find_element_by_id("passwd");
 element.send_keys(pass_ward)
 time.sleep(3)
 driver.find_element_by_id('btnSubmit').click();
-time.sleep(10)
+time.sleep(4)
+driver.find_element_by_id('back_originalpage').click();
 
-driver.quit()
+# 画像データを引っ張ってくる
+
+target_dir = 'images/active'
+time.sleep(1)
+shutil.rmtree(target_dir)
+os.mkdir(target_dir)
+time.sleep(1)
+new_path = shutil.move('images/stock/image' + "1", 'images/active/')
+time.sleep(1)
+pyautogui.click(x=300, y=300)
+time.sleep(1)
+
+
+# データをドロップする
+pyautogui.moveTo(1, 1, duration=0)
+time.sleep(0.5)
+screen_x,screen_y = pyautogui.size()
+pyautogui.moveRel(1370, 40, 0)
+time.sleep(0.5)
+pyautogui.dragTo(400, 400, 0.5, button='left')
+
+
+
+
+
+# driver.quit()
 
 # element = driver.find_element_by_id("twotabsearchtextbox")
 # element.submit()
