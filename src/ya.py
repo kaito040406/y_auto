30089c7bf1b0ef99542bc8856ce7d4e707f7b3ed
kaito hasegawa <kaitohasegawa000@gmail.com>
2020-04-15 19:51:01 +0900

    修正

--- src/ya.py
+++ src/ya.py
@@ -1,7 +1,18 @@
+# ブラウザ操作
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
-import os, sys, time
+
+##PyAutoGUIのモジュール
+import pyautogui
+
+##プロセスを制御するためにOS周りのモジュール
+import re
+import os
+import subprocess
+import sys
+import time
+import array
 
 
 # ブラウザ操作
@@ -9,7 +20,7 @@ mail_adress = "akitan_neru@yahoo.co.jp"
 pass_ward = "Hamagonn5"
 options = Options()
 driver = webdriver.Chrome(chrome_options=options)
-url = "https://login.yahoo.co.jp/config/login?auth_lv=pw&.lg=jp&.intl=jp&.src=auc&.done=https%3A%2F%2Fauctions.yahoo.co.jp%2F&sr_required=birthday%20gender%20postcode%20deliver"
+url = "https://auctions.yahoo.co.jp/sell/jp/show/submit?category=0"
 driver.get(url)
 time.sleep(5)
 # driver.find_element_by_class_name('t_jb9bKlgIcajcRS2hZAP').click();
